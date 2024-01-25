8651b061f2ab85b0dc3b61c7cb37a62ab7a07b7a
genta <gentaalimapersada@upi.edu>
2024-01-26 02:00:03 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    listStyleType: {
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal',
+      square: 'square',
+      roman: 'upper-roman',
+      circle: 'circle',
+    },
+    extend: {
+      colors:{
+        black: '#0f172a',
+      },
+    },
+  },
+  plugins: [],
+}
+
